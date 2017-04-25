00c7f029dbeb4e05b6f037e8938d4001c71e320b
IdrisMiles <idris.miles@gmail.com>
2017-04-25 19:44:22 +0100

    updating travis

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
-# sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
-# sudo apt-get update -q
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+sudo apt-get update -q
+sudo apt-get install g++-4.8 -y
 
-# sudo apt-get install g++-4.8 -y
 
-
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y 
+# sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y 
 # sudo apt-get -qq update -y
 # sudo apt-get upgrade -y
 # sudo apt-get dist-upgrade -y
