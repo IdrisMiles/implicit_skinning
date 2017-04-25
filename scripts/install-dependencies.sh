93a10d48ddfd790e70ab3ce5402f6b67d070de92
IdrisMiles <idris.miles@gmail.com>
2017-04-25 21:15:42 +0100

    updating travis

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,22 +1,13 @@
 #!/usr/bin/env bash
 
-# sudo apt install ppa-purge
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
-sudo apt-get update -q
-sudo apt-get -qq install g++-4.8 -y
-sudo apt-get -qq install gcc-4.8 -y
 
-# sudo apt-get update -qq
-# sudo apt-get install -qq g++-4.8
+# sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+# sudo apt-get update -q
+# sudo apt-get -qq install g++-4.8 -y
+# sudo apt-get -qq install gcc-4.8 -y
 
-# sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y 
-# sudo apt-get -qq update -y
-# sudo apt-get upgrade -y
-# sudo apt-get dist-upgrade -y
-sudo apt-get -qq install libstdc++6 -y
-# sudo apt-get -f install
+# sudo apt-get -qq install libstdc++6 -y
 
-# if [ "$CXX" = "g++" ]; then export CXX="g++-4.9" CC="gcc-4.9"; fi
 
 sudo apt-get install libgtest-dev
 sudo apt-get install cmake
