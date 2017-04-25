96c4027b76dd817014cb175e0c9731504322f856
IdrisMiles <idris.miles@gmail.com>
2017-04-25 19:12:49 +0100

    updating travis

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -6,9 +6,9 @@
 # sudo apt-get install g++-4.8 -y
 
 sudo apt-get install libstdc++6 -y
-# sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y 
-# sudo apt-get update -y
-# sudo apt-get upgrade -y
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y 
+sudo apt-get -qq update -y
+sudo apt-get upgrade -y
 # sudo apt-get dist-upgrade -y
 
 sudo apt-get install libgtest-dev
