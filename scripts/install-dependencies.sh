f16845ac7a621270c276af8fd0f576c3417b7876
IdrisMiles <idris.miles@gmail.com>
2017-04-25 17:54:25 +0100

    updating travis

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-# sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
-# sudo apt-get update -q
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+sudo apt-get update -q
 
-# sudo apt-get install g++-5 -y
+sudo apt-get install g++-5 -y
 
 sudo apt-get install libgtest-dev
 sudo apt-get install cmake
