a530589717f0738820999c719e7209c8d1cd3476
IdrisMiles <idris.miles@gmail.com>
2017-04-25 16:03:52 +0100

    adding travis

---
+++ scripts/build-tests.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+cd test/Mesh
+qmake
+make clean
+make
+cd ../bin
+./TestMesh
---
+++ scripts/install-dependencies.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+git clone https://github.com/g-truc/glm.git
+cd glm
+git checkout 0.9.8.0 -b glm
+mkdir build
+cd build
+cmake ..
+make
+cd ../../
+
+sudo apt-get install sudo apt-get install qt5-qmake
